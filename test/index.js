3909b72ce33d3dfc86e285b147cf0686edde95cc
Adam Brady <adam@boxxen.org>
2016-07-08 11:56:28 +1000

    standard

--- test/index.js
+++ test/index.js
@@ -1,271 +1,249 @@
+/* eslint-env node, mocha */
+var assert = require('assert')
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('supertest')
+var ht = require('hudson-taylor')
+var s = require('ht-schema')
 
-var assert     = require('assert');
-var express    = require('express');
-var bodyParser = require('body-parser');
-var request    = require('supertest');
-var ht         = require('hudson-taylor');
-var s          = require('ht-schema');
+var hte = require('../')
 
-var hte = require('../');
+describe('HT Express', function () {
+  it('should return response from method', function (done) {
+    var client = getClientWithServices({ s1: { m1: function (data, callback) {
+      return callback(null, 'hello world')
+    } } })
 
-describe("HT Express", function() {
-
-  it("should return response from method", function(done) {
-
-    var client = getClientWithServices({ s1: { m1: function(data, callback) {
-      return callback(null, 'hello world');
-    } } });
-
-    var app = express();
-    app.get('/', hte(client, "s1", "m1"));
+    var app = express()
+    app.get('/', hte(client, 's1', 'm1'))
 
     request(app)
       .get('/')
       .expect(200)
       .expect('hello world')
-      .end(done);
+      .end(done)
+  })
 
-  });
+  it('should return correct json headers if response is an object', function (done) {
+    var client = getClientWithServices({ s1: { m1: function (data, callback) {
+      return callback(null, data)
+    } } })
 
-  it("should return correct json headers if response is an object", function(done) {
-
-    var client = getClientWithServices({ s1: { m1: function(data, callback) {
-      return callback(null, data);
-    } } });
-
-    var app = express();
-    app.use(bodyParser.json());
-    app.get('/', hte(client, "s1", "m1"));
+    var app = express()
+    app.use(bodyParser.json())
+    app.get('/', hte(client, 's1', 'm1'))
 
     var data = {
-      hello: "world"
-    };
+      hello: 'world'
+    }
 
     request(app)
       .get('/')
       .expect(200)
-      .expect("Content-Type", "application/json; charset=utf-8")
+      .expect('Content-Type', 'application/json; charset=utf-8')
       .send(data)
-      .end(function(err, res) {
-        assert.ifError(err);
-        assert.deepEqual(res.body, data);
-        done();
-    });
+      .end(function (err, res) {
+        assert.ifError(err)
+        assert.deepEqual(res.body, data)
+        done()
+      })
+  })
 
-  });
-
-  it("should return custom code if statusCode is set in response", function(done) {
-
-     var client = getClientWithServices({ s1: { m1: function(data, callback) {
+  it('should return custom code if statusCode is set in response', function (done) {
+    var client = getClientWithServices({ s1: { m1: function (data, callback) {
       return callback(null, {
         statusCode: 201,
         something: 'else'
-      });
-    } } });
+      })
+    } } })
 
-    var app = express();
-    app.post('/', hte(client, "s1", "m1"));
+    var app = express()
+    app.post('/', hte(client, 's1', 'm1'))
 
     request(app)
       .post('/')
       .expect(201)
       .expect({ something: 'else' })
-      .end(done);
+      .end(done)
+  })
 
-  });
-
-  it("should only pull data from body by default", function(done) {
-
-    var client = getClientWithServices({ s1: { m1: function(data, callback) {
+  it('should only pull data from body by default', function (done) {
+    var client = getClientWithServices({ s1: { m1: function (data, callback) {
       assert.deepEqual(data, {
         hello: 'world'
-      });
-      done();
-    } } });
+      })
+      done()
+    } } })
 
-    var app = express();
-    app.use(bodyParser.json());
-    app.post('/', hte(client, "s1", "m1"));
+    var app = express()
+    app.use(bodyParser.json())
+    app.post('/', hte(client, 's1', 'm1'))
 
     request(app)
       .post('/')
       .query({
-        not: "used"
+        not: 'used'
       })
       .send({
-        hello: "world"
+        hello: 'world'
       })
       .expect(200)
-      .end();
+      .end()
+  })
 
-  });
-
-  it("should pull data from other places if specified", function(done) {
-
-    var client = getClientWithServices({ s1: { m1: function(data, callback) {
+  it('should pull data from other places if specified', function (done) {
+    var client = getClientWithServices({ s1: { m1: function (data, callback) {
       assert.deepEqual(data, {
         hello: 'world',
-        is:    'used'
-      });
-      done();
-    } } });
+        is: 'used'
+      })
+      done()
+    } } })
 
-    var app = express();
-    app.use(bodyParser.json());
-    app.post('/', hte(client, "s1", "m1", [ 'body', 'query' ]));
+    var app = express()
+    app.use(bodyParser.json())
+    app.post('/', hte(client, 's1', 'm1', [ 'body', 'query' ]))
 
     request(app)
       .post('/')
       .query({
-        is: "used"
+        is: 'used'
       })
       .send({
-        hello: "world"
+        hello: 'world'
       })
       .expect(200)
-      .end();
+      .end()
+  })
 
-  });
-
-  it("should merge data properly", function(done) {
-
-    var client = getClientWithServices({ s1: { m1: function(data, callback) {
+  it('should merge data properly', function (done) {
+    var client = getClientWithServices({ s1: { m1: function (data, callback) {
       assert.deepEqual(data, {
-        one:   1,
-        two:   22,
+        one: 1,
+        two: 22,
         three: 333
-      });
-      done();
-    } } });
+      })
+      done()
+    } } })
 
-    var app = express();
-    app.use(bodyParser.json());
-    app.post('/:three', hte(client, "s1", "m1", [ 'body', 'query', 'params' ]));
+    var app = express()
+    app.use(bodyParser.json())
+    app.post('/:three', hte(client, 's1', 'm1', [ 'body', 'query', 'params' ]))
 
     request(app)
       .post('/333')
       .query({
-        two:   22,
+        two: 22,
         three: 33
       })
       .send({
-        one:   1,
-        two:   2,
+        one: 1,
+        two: 2,
         three: 3
       })
       .expect(200)
-      .end();
+      .end()
+  })
 
-  });
-
-  it("should return 400 if there was a validation error", function(done) {
-
+  it('should return 400 if there was a validation error', function (done) {
     var client = getClientWithServices({ s1: { m1: {
-      fn: function(data, callback) {
-        return callback();
+      fn: function (data, callback) {
+        return callback()
       },
       schema: s.Number()
-    } } });
+    } } })
 
-    var app = express();
-    app.get('/', hte(client, "s1", "m1"));
+    var app = express()
+    app.get('/', hte(client, 's1', 'm1'))
 
     request(app)
       .get('/')
       .expect(400)
-      .end(function(err, res) {
-        assert.ifError(err);
-        assert.equal(res.text, 'required Number, received object');
-        done();
-    });
+      .end(function (err, res) {
+        assert.ifError(err)
+        assert.equal(res.text, 'required Number, received object')
+        done()
+      })
+  })
 
-  });
-
-  it("should return 500 if statusCode is not set in error", function(done) {
-
-    var client = getClientWithServices({ s1: { m1: function(data, callback) {
+  it('should return 500 if statusCode is not set in error', function (done) {
+    var client = getClientWithServices({ s1: { m1: function (data, callback) {
       return callback({
         something: 'else'
-      });
-    } } });
+      })
+    } } })
 
-    var app = express();
-    app.post('/', hte(client, "s1", "m1"));
+    var app = express()
+    app.post('/', hte(client, 's1', 'm1'))
 
     request(app)
       .post('/')
       .expect(500)
-      .end(done);
+      .end(done)
+  })
 
-  });
-
-  it("should return custom code if statusCode is set in error", function(done) {
-
-    var client = getClientWithServices({ s1: { m1: function(data, callback) {
+  it('should return custom code if statusCode is set in error', function (done) {
+    var client = getClientWithServices({ s1: { m1: function (data, callback) {
       return callback({
         statusCode: 501,
         something: 'else'
-      });
-    } } });
+      })
+    } } })
 
-    var app = express();
-    app.post('/', hte(client, "s1", "m1"));
+    var app = express()
+    app.post('/', hte(client, 's1', 'm1'))
 
     request(app)
       .post('/')
       .expect(501)
-      .end(done);
+      .end(done)
+  })
 
-  });
+  it('should return 500 if error is not an object', function (done) {
+    var client = getClientWithServices({ s1: { m1: function (data, callback) {
+      return callback('error')
+    } } })
 
-  it("should return 500 if error is not an object", function(done) {
-
-    var client = getClientWithServices({ s1: { m1: function(data, callback) {
-      return callback("error");
-    } } });
-
-    var app = express();
-    app.get('/', hte(client, "s1", "m1"));
+    var app = express()
+    app.get('/', hte(client, 's1', 'm1'))
 
     request(app)
       .get('/')
       .expect(500)
-      .end(function(err, res) {
-        assert.ifError(err);
-        assert.equal(res.text, "error");
-        done();
-    });
+      .end(function (err, res) {
+        assert.ifError(err)
+        assert.equal(res.text, 'error')
+        done()
+      })
+  })
+})
 
-  });
-
-});
-
-function getClientWithServices(services) {
+function getClientWithServices (services) {
   /*
     services should look something like:
     {
       service1: {
-        method1: function(data, callback) {
-  
+        method1: function (data, callback) {
+
         },
         method2: {
-          fn: function(data, callback) {},
+          fn: function (data, callback) {},
           schema: s.String()
         }
       },
       service2: ..etc
     }
   */
-  var client = new ht.Client();
-  for(var service in services) {
-    var t = new ht.Transports.Local();
-    var s = new ht.Service(t);
-    for(var method in services[service]) {
-      var p = services[service][method];
-      if(typeof p === 'function') p = { fn: p };
-      s.on(method, p.schema, p.fn);
+  var client = new ht.Client()
+  for (var service in services) {
+    var t = new ht.Transports.Local()
+    var s = new ht.Service(t)
+    for (var method in services[service]) {
+      var p = services[service][method]
+      if (typeof p === 'function') p = { fn: p }
+      s.on(method, p.schema, p.fn)
     }
-    client.add(service, t);
+    client.add(service, t)
   }
-  return client;
+  return client
 }
